99b899aad2f8619f78029aae369ff5bdd7e41cc9
Susmita <ssaha7866@gmail.com>
2019-07-03 19:12:31 +0530

    c

--- jdbcapp/src/main/java/com/covalense/jdbcapp/common/ClassY.java
+++
@@ -1,9 +0,0 @@
-package com.covalense.jdbcapp.common;
-
-public class ClassY extends ClassX{
-
-	public ClassY(int n) {
-		super(n);
-	}
-
-}
--- jdbcapp/src/main/java/com/covalense/jdbcapp/common/Connection.java
+++
@@ -1,6 +0,0 @@
-package com.covalense.jdbcapp.common;
-
-public interface Connection {
-
-	void getConnection();
-}
